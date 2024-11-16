7b9062138b2f1bded3ba11faa476736b23136ecc
SyedSameed02 <syedsameed0201@gmail.com>
2024-11-16 20:01:29 +0530

    Initial commit

---
+++ src/main/java/com/projectSam/projects/UrlShortnerApplication.java
@@ -0,0 +1,13 @@
+package com.projectSam.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectSam/projects/UrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectSam.projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
