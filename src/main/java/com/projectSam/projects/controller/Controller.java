3a9161fa8e12b9242c717347c413241ab14e48cc
SyedSameed02 <syedsameed0201@gmail.com>
2024-11-17 10:34:52 +0530

    commited on 10:34 nov 17

--- src/main/java/com/projectSam/projects/controller/Controller.java
+++ src/main/java/com/projectSam/projects/controller/Controller.java
@@ -1,4 +1,28 @@
 package com.projectSam.projects.controller;
 
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
 public class Controller {
+
+
+    @RequestMapping("/api/test")
+    public  String print()
+    {
+        return  "CALLED";
+
+    }
+
+
+
+    @RequestMapping("/get")
+    public void getUrl(@RequestBody String string)
+    {
+        System.out.println(string);
+    }
+
+
 }
