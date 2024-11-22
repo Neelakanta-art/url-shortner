da473e26b9990df146718a36b6a5f5f4bc4248b1
SyedSameed02 <syedsameed0201@gmail.com>
2024-11-22 15:43:10 +0530

    B

--- src/main/java/com/projectSam/projects/controller/Controller.java
+++ src/main/java/com/projectSam/projects/controller/Controller.java
@@ -1,10 +1,12 @@
 package com.projectSam.projects.controller;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController("/")
 public class Controller {
 
--- src/main/java/com/projectSam/projects/entity/UrlEntity.java
+++ src/main/java/com/projectSam/projects/entity/UrlEntity.java
@@ -1,26 +1,4 @@
 package com.projectSam.projects.entity;
 
-
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.time.LocalDateTime;
-
-@Document(collection = "urls")
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 public class UrlEntity {
-
-    @Id
-    private String id;
-
-    private String url;
-
-    @Indexed(expireAfterSeconds = 0)
-    private LocalDateTime expiresAt;
 }
